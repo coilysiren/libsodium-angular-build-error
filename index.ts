67d19bba7ea506a69c9b8fb942ffba7c81b3ea5c
Lynn Cyrin <firemagelynn@gmail.com>
2018-05-06 09:05:33 -0700

    unused code

--- index.ts
+++ index.ts
@@ -2,5 +2,4 @@ import * as _uninitalizedSodiumDoNotUse from "libsodium-wrappers";
 
 export async function readySodium(): Promise<void> {
   await _uninitalizedSodiumDoNotUse.ready;
-  const sodium: any = undefined;
 }
